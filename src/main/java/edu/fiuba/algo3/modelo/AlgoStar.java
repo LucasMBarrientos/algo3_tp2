19ea817586361db231dcba2e327e6c8860f301e0
amurseli <86434411+amurseli@users.noreply.github.com>
2022-11-04 20:10:37 -0300

    Primera tirada de clases
    Pair programming con todos

---
+++ src/main/java/edu/fiuba/algo3/modelo/AlgoStar.java
@@ -0,0 +1,18 @@
+package edu.fiuba.algo3.modelo;
+
+public class AlgoStar {
+
+    public void empezarJuego(){
+
+    }
+
+    public void generarZangano(){
+
+    }
+
+    public void construirCriadero(){
+        //jugador.construirCriadero
+    }
+
+
+}
---
+++ src/main/java/edu/fiuba/algo3/modelo/JugadorZerg.java
@@ -0,0 +1,11 @@
+package edu.fiuba.algo3.modelo;
+
+public class JugadorZerg {
+
+    //private Zangano[] zanganosDisponibles;
+
+    public void construirCriadero(){
+
+      //  edificios.add()
+    }
+}
