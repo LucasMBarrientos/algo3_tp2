81e8ff7b8e69ec9b218fa2f311c666fb26ea3965
amurseli <86434411+amurseli@users.noreply.github.com>
2022-11-09 01:20:03 -0300

    Create CasoDeUso3.java

---
+++ src/main/test/edu/fiuba/algo3/entrega_1/CasoDeUso3.java
@@ -0,0 +1,11 @@
+package edu.fiuba.algo3.entrega_1;
+
+import org.junit.jupiter.api.Test;
+
+public class CasoDeUso3 {
+
+    @Test
+    public void verificarQueUnaCasillaTieneUnVolcan(){
+
+    }
+}
