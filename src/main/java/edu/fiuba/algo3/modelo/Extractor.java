08f9a56d62b8bf56488c3daf10a009d9ef27bcd5
amurseli <86434411+amurseli@users.noreply.github.com>
2022-11-09 19:58:03 -0300

    Revert "intento de caso 4"
    This reverts commit c13b9514422583a7e5f6b7f5716af5d0f0528e09.

--- src/main/java/edu/fiuba/algo3/modelo/Extractor.java
+++ src/main/java/edu/fiuba/algo3/modelo/Extractor.java
@@ -1,8 +1,6 @@
 package edu.fiuba.algo3.modelo;
 
 public class Extractor extends Edificio {
-
-    public int zanganosTrabajando = 0;
     
     public Extractor() {
         this.tiempoConstruccion = 6;
