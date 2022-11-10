0b8de136749e3034e7f44cfa023d1d50673b8b43
Ian Anca <iananca104@gmail.com>
2022-11-09 23:20:33 -0300

    Add files via upload

---
+++ src/main/test/edu/fiuba/algo3/entrega_1/CasoDeUso15.java
@@ -0,0 +1,35 @@
+package edu.fiuba.algo3.entrega_1;
+
+import edu.fiuba.algo3.modelo.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CasoDeUso16 {
+    
+    @Test
+    public void noSePuedeConstruirUnExtractorSobreOtroExtractor() {
+        AlgoStar algoStar = new AlgoStar();
+        algoStar.empezarJuego();
+        algoStar.generarUnidad(1,1);
+        algoStar.moverDerecha(1,1);
+        algoStar.seleccionarCasilla(2,1).establecerTerreno(new Volcan());
+        algoStar.construirEdificio(2,1, new Extractor());
+        algoStar.pasarTurno();
+        algoStar.pasarTurno();
+        algoStar.pasarTurno();
+        algoStar.pasarTurno();
+        algoStar.pasarTurno();
+        Assertions.assertFalse(algoStar.seleccionarCasilla(2,1).devolverEdificio() instanceof Extractor);
+        algoStar.pasarTurno();
+        Assertions.assertTrue(algoStar.seleccionarCasilla(2,1).devolverEdificio() instanceof Extractor); // Extractor fue construido en (2,1)
+        algoStar.generarUnidad(1,1);
+        algoStar.moverDerecha(1,1);
+        algoStar.seleccionarCasilla(1,1).ponerUnidadATrabajar();
+        // Algostar.ponerUnidadATrabajar(int x, int y) = {
+        // Trabajador unidadTrabajadora = (Trabajador)this.mapa.buscarCasilla(x,y).devolverUnidad();
+        // Trabajable edificioTrabajable = (Trabajable)this.mapa.buscarCasilla(x,y).devolverEdificio();
+        // unidadTrabajadora.trabajar(EdificioTrabajable edificioTrabajable) 
+        // }
+    }
+
+}
