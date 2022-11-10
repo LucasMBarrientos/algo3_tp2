b2be244ba5e780b88e35c761952f8ceb287882a4
amurseli <amurseli@fi.uba.ar>
2022-11-10 17:09:47 -0300

    Caso de Uso 14

---
+++ src/main/test/edu/fiuba/algo3/entrega_1/CasoDeUso14.java
@@ -0,0 +1,49 @@
+package edu.fiuba.algo3.entrega_1;
+
+import edu.fiuba.algo3.modelo.AlgoStar;
+import edu.fiuba.algo3.modelo.edificios.protoss.Pilon;
+import edu.fiuba.algo3.modelo.terrenos.TerrenoEnergizado;
+import edu.fiuba.algo3.modelo.terrenos.TerrenoMoho;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CasoDeUso14 {
+
+    @Test
+    public void MohoNoPisaEnergizado(){
+        AlgoStar algoStar = new AlgoStar();
+        algoStar.empezarJuego();
+
+        Assertions.assertTrue(algoStar.seleccionarCasilla(8,9).devolverTerreno() instanceof TerrenoEnergizado);
+        for (int i = 0; i < 20; i++) {
+            algoStar.pasarTurno();
+        }
+        Assertions.assertTrue(algoStar.seleccionarCasilla(8,9).devolverTerreno() instanceof TerrenoEnergizado);
+
+    }
+
+    @Test
+    public void EnergizadoNoPisaMoho(){
+        AlgoStar algoStar = new AlgoStar();
+        algoStar.empezarJuego();
+
+
+        for (int i = 0; i < 20; i++) {
+            algoStar.pasarTurno();
+        }
+        Assertions.assertTrue(algoStar.seleccionarCasilla(7,6).devolverTerreno() instanceof TerrenoMoho);
+        algoStar.construirEdificio(7,8,new Pilon());
+        for (int i = 0; i < 10; i++) {
+            algoStar.pasarTurno();
+        }
+        Assertions.assertTrue(algoStar.seleccionarCasilla(7,6).devolverTerreno() instanceof TerrenoMoho);
+
+        algoStar.construirEdificio(7,6,new Pilon());
+
+        for (int i = 0; i < 10; i++) {
+            algoStar.pasarTurno();
+        }
+        Assertions.assertFalse(algoStar.seleccionarCasilla(7,6).devolverEdificio() instanceof Pilon);
+
+    }
+}
