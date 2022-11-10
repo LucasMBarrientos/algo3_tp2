29f7bbfa45a777f47dc0d863c43f1a863bfa410b
Lucas Barrientos <lucasmabarrientos@gmail.com>
2022-11-10 01:07:51 -0300

    caso 7

---
+++ src/main/test/edu/fiuba/algo3/entrega_1/CasoDeUso7.java
@@ -0,0 +1,77 @@
+package edu.fiuba.algo3.entrega_1;
+
+import edu.fiuba.algo3.modelo.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import edu.fiuba.algo3.modelo.edificios.protoss.NexoMineral;
+import edu.fiuba.algo3.modelo.edificios.zerg.Criadero;
+
+public class CasoDeUso7 {
+
+    @Test
+    public void verificarQueElNexoMineralSeaConstruibleSobreUnMineral() {
+      AlgoStar algoStar = new AlgoStar();
+      algoStar.empezarJuego();
+
+      algoStar.seleccionarCasilla(2, 1).establecerTerreno(new TerrenoMineral());
+      algoStar.construirEdificio(2, 1, new NexoMineral());
+
+      algoStar.pasarTurno();
+      algoStar.pasarTurno();
+      algoStar.pasarTurno();
+      algoStar.pasarTurno();
+
+      Assertions.assertTrue(algoStar.seleccionarCasilla(2, 1).devolverEdificio() instanceof NexoMineral);
+    }
+
+    @Test
+    public void verificarQueElNexoMineralSoloSeaConstruibleSobreUnTerrenoMineral() {
+      AlgoStar algoStar = new AlgoStar();
+      algoStar.empezarJuego();
+
+      algoStar.construirEdificio(2, 1, new NexoMineral());
+
+      algoStar.pasarTurno();
+      algoStar.pasarTurno();
+      algoStar.pasarTurno();
+      algoStar.pasarTurno();
+
+      Assertions.assertFalse(algoStar.seleccionarCasilla(2, 1).devolverEdificio() instanceof NexoMineral);
+    }
+
+    @Test
+    public void verificarQueProtosPuedaRecolectarMinerales() {
+      AlgoStar algoStar = new AlgoStar();
+      algoStar.empezarJuego();
+
+      algoStar.seleccionarCasilla(2, 1).establecerTerreno(new TerrenoMineral());
+      algoStar.construirEdificio(2, 1, new NexoMineral());
+
+      algoStar.pasarTurno();
+      algoStar.pasarTurno();
+      algoStar.pasarTurno();
+      algoStar.pasarTurno();
+
+      Assertions.assertTrue(algoStar.cantidadMinerales() == 0);
+      algoStar.pasarTurno();
+      Assertions.assertTrue(algoStar.cantidadMinerales() == 1);
+    }
+
+    @Test
+    public void verificarQueZergPuedaRecolectarMinerales() {
+      AlgoStar algoStar = new AlgoStar();
+      algoStar.empezarJuego();
+      algoStar.generarUnidad(1, 1);
+      Unidad zanganoDisponible = algoStar.seleccionarCasilla(1, 1).devolverUnidad();
+
+      algoStar.moverDerecha(1, 1);
+      algoStar.seleccionarCasilla(2, 1).establecerTerreno(new TerrenoMineral());
+      
+      // PONER A TRABAJAR UN ZANGANO SOBRE UN MINERAL?
+
+      Assertions.assertTrue(algoStar.cantidadMinerales() == 0);
+      algoStar.pasarTurno();
+      Assertions.assertTrue(algoStar.cantidadMinerales() == 1);
+    }
+}
