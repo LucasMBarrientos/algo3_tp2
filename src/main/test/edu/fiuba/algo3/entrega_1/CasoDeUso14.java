733c1586dbadd178b7b19d1d7a7e4dc14ebb34dc
eze <eze.f.tec@gmail.com>
2022-11-18 00:15:31 -0300

    :(

--- src/main/test/edu/fiuba/algo3/entrega_1/CasoDeUso14.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/CasoDeUso14.java
@@ -1,14 +1,45 @@
 
+
 package edu.fiuba.algo3.entrega_1;
 
-import edu.fiuba.algo3.modelo.AlgoStar;
-import edu.fiuba.algo3.modelo.edificios.protoss.pilon.Pilon;
-import edu.fiuba.algo3.modelo.terrenos.TerrenoEnergizado;
-import edu.fiuba.algo3.modelo.terrenos.TerrenoMoho;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+        import edu.fiuba.algo3.modelo.AlgoStar;
+        import edu.fiuba.algo3.modelo.JugadorProtoss;
+        import edu.fiuba.algo3.modelo.unidades.edificios.protoss.pilon.Pilon;
+        import edu.fiuba.algo3.modelo.terrenos.TerrenoMoho;
+        import org.junit.jupiter.api.Assertions;
+        import org.junit.jupiter.api.Test;
 
-public class CasoDeUso14 { /*
+    public class CasoDeUso14 {/*
+        @Test
+        public void EnergizadoNoPisaMoho() {
+            AlgoStar algoStar = new AlgoStar();
+            JugadorZerg jugadorZerg = new JugadorZerg("La mente suprema", "#ff0000", 325, 0);
+            algoStar.agregarJugador(jugadorZerg);
+            JugadorProtoss jugadorProtoss = new JugadorProtoss("El primogenito", "#0000ff");
+            algoStar.agregarJugador(jugadorProtoss);
+            algoStar.empezarJuego();
+            // Se construye un criadero
+            jugadorZerg.construirEdificio(new Coordenada(2,2), new Criadero());
+            jugadorProtoss.construirEdificio(new Coordenada(4,4), new Pilon());
+            for (int i = 0; i < 8; i++) {
+                algoStar.pasarTurno();
+            }
+
+
+            //Assertions.assertThrows(EdificioDestruido.class, pilon::actualizar); //TODO agregar state edificio destruido @Leti
+            Assertions.assertThrows(TerrenoNoAptoParaConstruirEsteEdificio.class, ()->{
+                jugadorProtoss.construirEdificio(new Coordenada(15,2), new pilon());
+            });
+        }
+    */
+
+
+    }
+
+
+
+
+    /*
 
     @Test
     public void MohoNoPisaEnergizado(){
---
+++ src/main/test/edu/fiuba/algo3/entrega_2/CasoDeUso21.java
@@ -0,0 +1,51 @@
+
+
+        import edu.fiuba.algo3.modelo.AlgoStar;
+        import edu.fiuba.algo3.modelo.Coordenada;
+        import edu.fiuba.algo3.modelo.JugadorProtoss;
+        import edu.fiuba.algo3.modelo.JugadorZerg;
+        import edu.fiuba.algo3.modelo.direcciones.Derecha;
+        import edu.fiuba.algo3.modelo.estadisticas.Danio;
+        import edu.fiuba.algo3.modelo.estadisticas.EdificioDestruido;
+        import edu.fiuba.algo3.modelo.unidades.edificios.protoss.pilon.Pilon;
+        import edu.fiuba.algo3.modelo.unidades.edificios.zerg.criadero.Criadero;
+        import edu.fiuba.algo3.modelo.unidades.edificios.zerg.espiral.Espiral;
+        import edu.fiuba.algo3.modelo.unidades.edificios.zerg.guarida.Guarida;
+        import edu.fiuba.algo3.modelo.unidades.zerg.Hidralisco;
+        import edu.fiuba.algo3.modelo.unidades.zerg.Mutalisco;
+        import org.junit.jupiter.api.Assertions;
+        import org.junit.jupiter.api.Test;
+
+public class CasoDeUso21 {/*
+
+    @Test
+    public void unMutaliscoNoPuedeEvolucionarAGuardiaSinLosRecursosSolicitados() {
+
+        AlgoStar algoStar = new AlgoStar();
+        JugadorZerg jugadorZerg = new JugadorZerg("La mente suprema", "#ff0000", 325, 0);
+        algoStar.agregarJugador(jugadorZerg);
+        JugadorProtoss jugadorProtoss = new JugadorProtoss("El primogenito", "#0000ff");
+        algoStar.agregarJugador(jugadorProtoss);
+        algoStar.empezarJuego();
+        jugadorZerg.construirEdificio(new Coordenada(4, 4), new Criadero());
+        for (int i = 0; i < 8; i++) {
+            algoStar.pasarTurno();
+        }
+        jugadorZerg.construirEdificio(new Coordenada(4, 5), new Guarida());
+        for (int i = 0; i < 8; i++) {
+            algoStar.pasarTurno();
+        }
+        jugadorZerg.construirEdificio(new Coordenada(5, 5), new Espiral());
+        for (int i = 0; i < 8; i++) {
+            algoStar.pasarTurno();
+        }
+        jugadorZerg.generarUnidad(new Coordenada(2, 2), new Derecha(), new Mutalisco());
+        for (int i = 0; i < 14; i++) { // tarda 7
+            algoStar.pasarTurno();
+        }
+        Assertions.assertTrue(jugadorZerg.evolucionar(new Coordenada(5, 5);
+    }
+
+
+*/
+}
