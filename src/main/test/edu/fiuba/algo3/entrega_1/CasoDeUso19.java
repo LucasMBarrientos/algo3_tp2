873d584f91f6b189cafa7816ecd576de64811d7f
Lucas Barrientos <lucasmabarrientos@gmail.com>
2022-11-17 22:36:40 -0300

    test: caso 19

---
+++ src/main/test/edu/fiuba/algo3/entrega_1/CasoDeUso19.java
@@ -0,0 +1,43 @@
+package edu.fiuba.algo3.entrega_1;
+
+public class CasoDeUso19 {
+    /*@Test
+    public void zerlingPuedeAtacarAZealotPeroNoAScout() {
+      Zerling zerling = new Zerling();
+      Zealot zealot = new Zealot();
+      Scout scout = new Scout();
+
+      zerling.atacar(zealot);
+
+      Assertions.assertThrows(AtaqueInvalido.class, ()->{
+        zerling.atacar(scout);
+      });
+    }
+
+    @Test
+    public void GuardianPuedeAtacarAZealotPeroNoAScout() {
+      Guardian guardian = new Guardian();
+      Zealot zealot = new Zealot();
+      Scout scout = new Scout();
+
+      guardian.atacar(zealot);
+
+      Assertions.assertThrows(AtaqueInvalido.class, ()->{
+        guardian.atacar(scout);
+      });
+    }
+
+    @Test
+    public void ZealotPuedeAtacarAHidraliscoPeroNoAMutalisco() {
+      Zealot zealot = new Zealot();
+      Hidralisco hidralisco = new Hidralisco();
+      Mutalisco mutalisco = new Mutalisco();
+
+      zealot.atacar(hidralisco);
+
+      Assertions.assertThrows(AtaqueInvalido.class, ()->{
+        zealot.atacar(mutalisco);
+      });
+    }*/
+
+}
