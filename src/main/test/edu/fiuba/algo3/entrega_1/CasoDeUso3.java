32459666cf4e7c8a9d01aa1b63a6999633c6e171
Lucas Barrientos <lucasmabarrientos@gmail.com>
2022-11-20 18:54:49 -0300

    caso 3 anda

--- src/main/test/edu/fiuba/algo3/entrega_1/CasoDeUso3.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/CasoDeUso3.java
@@ -1,52 +1,57 @@
 package edu.fiuba.algo3.entrega_1;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import edu.fiuba.algo3.modelo.edificios.protoss.asimilador.Asimilador;
+import edu.fiuba.algo3.modelo.edificios.protoss.pilon.Pilon;
+import edu.fiuba.algo3.modelo.edificios.zerg.criadero.Criadero;
+import edu.fiuba.algo3.modelo.edificios.zerg.extractor.Extractor;
+import edu.fiuba.algo3.modelo.excepciones.TerrenoNoAptoParaConstruirTalEdificio;
+import edu.fiuba.algo3.modelo.geometria.Coordenada;
+import edu.fiuba.algo3.modelo.terrenos.Terreno;
+import edu.fiuba.algo3.modelo.terrenos.TerrenoBase;
+import edu.fiuba.algo3.modelo.terrenos.TerrenoVolcan;
 
 public class CasoDeUso3 {
-  /*
-    @Test
-    public void asimilidarSePuedeConstruirSobreTerrenoVolcan() {
-      Terreno terrenoVolcan = new TerrenoVolcan();
-      Asimilador asimilador = new Asimilador();
-
-      Assert.assertNotNull(terrenoVolcan.construir(asimilador));
-    }
-
-    @Test
-    public void extractorSePuedeConstruirSobreTerrenoVolcan() {
-      Terreno terrenoVolcan = new TerrenoVolcan();
-      Extractor extractor = new Extractor();
-
-      Assert.assertNotNull(terrenoVolcan.construir(extractor));
-    }
-
     @Test
     public void extractorNoPuedeConstruirseSobreUnTerrenoNoVolcan() {
-      Terreno terreno = new Terreno();
+      Terreno terreno = new TerrenoBase(new Coordenada( 1,1));
       Extractor extractor = new Extractor();
 
-      Assertions.assertThrows(TerrenoNoAptoParaConstruirEsteEdificio.class, ()->{
-        terreno.construir(extractor);
+      Assertions.assertThrows(TerrenoNoAptoParaConstruirTalEdificio.class, ()->{
+        extractor.ocupar(terreno);
       });
     }
 
     @Test
     public void asimilidarNoPuedeConstruirseSobreUnTerrenoNoVolcan() {
-      Terreno terreno = new Terreno();
+      Terreno terreno = new TerrenoBase(new Coordenada( 1,1));
       Asimilador asimilador = new Asimilador();
 
-      Assertions.assertThrows(TerrenoNoAptoParaConstruirEsteEdificio.class, ()->{
-        terreno.construir(asimilador);
+      Assertions.assertThrows(TerrenoNoAptoParaConstruirTalEdificio.class, ()->{
+        asimilador.ocupar(terreno);
       });
     }
 
     @Test
-    public void CriaderoNoSePuedeConstruirSobreTerrenoVolcan() {
-      Terreno terrenoVolcan = new TerrenoVolcan();
+    public void criaderoNoSePuedeConstruirSobreTerrenoVolcan() {
+      Terreno terrenoVolcan = new TerrenoVolcan(new Coordenada( 1,1));
       Criadero criadero = new Criadero();
 
-      Assertions.assertThrows(TerrenoNoAptoParaConstruirEsteEdificio.class, ()->{
-        terreno.construir(criadero);
+      Assertions.assertThrows(TerrenoNoAptoParaConstruirTalEdificio.class, ()->{
+        criadero.ocupar(terrenoVolcan);
       });
     }
-    */
+
+    @Test
+    public void pilonNoSePuedeConstruirSobreTerrenoVolcan() {
+      Terreno terrenoVolcan = new TerrenoVolcan(new Coordenada( 1,1));
+      Pilon pilon = new Pilon();
+
+      Assertions.assertThrows(TerrenoNoAptoParaConstruirTalEdificio.class, ()->{
+        pilon.ocupar(terrenoVolcan);
+      });
+    }
+    
 }
