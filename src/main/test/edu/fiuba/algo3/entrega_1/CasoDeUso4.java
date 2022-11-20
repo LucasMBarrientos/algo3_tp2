c014727748910aad7c25c7eac64864eb8c50bff5
Lucas Barrientos <lucasmabarrientos@gmail.com>
2022-11-20 19:03:00 -0300

    caso 4

--- src/main/test/edu/fiuba/algo3/entrega_1/CasoDeUso4.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/CasoDeUso4.java
@@ -1,9 +1,15 @@
 package edu.fiuba.algo3.entrega_1;
 
 import edu.fiuba.algo3.modelo.*;
+import edu.fiuba.algo3.modelo.edificios.protoss.asimilador.Asimilador;
 import edu.fiuba.algo3.modelo.edificios.zerg.espiral.Espiral;
 import edu.fiuba.algo3.modelo.edificios.zerg.extractor.Extractor;
+import edu.fiuba.algo3.modelo.excepciones.RecursosInsuficientes;
+import edu.fiuba.algo3.modelo.geometria.Coordenada;
+import edu.fiuba.algo3.modelo.jugadores.Inventario;
 import edu.fiuba.algo3.modelo.recursos.GasVespeno;
+import edu.fiuba.algo3.modelo.recursos.Mineral;
+import edu.fiuba.algo3.modelo.terrenos.Terreno;
 import edu.fiuba.algo3.modelo.terrenos.TerrenoVolcan;
 import edu.fiuba.algo3.modelo.unidades.zerg.Zangano;
 
@@ -14,68 +20,125 @@ import org.junit.jupiter.api.Test;
 
 public class CasoDeUso4 {
 
-    /*
-
+   
     @Test
     public void extractorCon0ZanganoGenera0UnidadesDeGas() {
-        Extractor extractor = new Extractor();
+      Terreno terrenoVolcan = new TerrenoVolcan(new Coordenada( 1,2));
+      Extractor extractor = new Extractor();
+      Inventario inv = new Inventario(new GasVespeno(0), new Mineral(0));
 
-        GasVespeno gas = extractor.extraerRecursos();
+      int tiempoDeConstruccion = 6;
+      extractor.ocupar(terrenoVolcan);
 
-        Assertions.assertThrows(RecursosInsuficientes.class,()->{
-            gas.gastar(new GasVespeno(10));
-        });
+      for (int i = 0; i < tiempoDeConstruccion; i++) {
+          extractor.actualizar();
+      }
+
+      //extractor.ingresarUnidad(new Zangano());
+      
+      extractor.recolectarRecursos(inv);
+
+      //inv.consumirGasVespeno(new GasVespeno(1));
+
+      Assertions.assertThrows(RecursosInsuficientes.class, ()->{
+          inv.consumirGasVespeno(new GasVespeno(1));
+      });
     }
 
     @Test
     public void extractorCon1ZanganoGenera10UnidadesDeGas() {
-        Extractor extractor = new Extractor(); // Aca podría faltar el terreno
-        Zangano zangano = new Zangano();
+      Terreno terrenoVolcan = new TerrenoVolcan(new Coordenada( 1,2));
+      Extractor extractor = new Extractor();
+      Inventario inv = new Inventario(new GasVespeno(0), new Mineral(0));
 
-        extractor.ingresarUnidad(zangano);
+      int tiempoDeConstruccion = 6;
+      extractor.ocupar(terrenoVolcan);
 
-        GasVespeno gas = extractor.extraerRecursos();
+      for (int i = 0; i < tiempoDeConstruccion; i++) {
+          extractor.actualizar();
+      }
 
-        Assertions.assertThrows(RecursosInsuficientes.class,()->{
-            gas.gastar(new GasVespeno(20));
-        });
+      extractor.ingresarUnidad(new Zangano());
+      
+      extractor.recolectarRecursos(inv);
+
+      inv.consumirGasVespeno(new GasVespeno(10));
+
+      Assertions.assertThrows(RecursosInsuficientes.class, ()->{
+          inv.consumirGasVespeno(new GasVespeno(1));
+      });
     }
 
     @Test
     public void extractorCon2ZanganoGenera20UnidadesDeGas() {
-        Extractor extractor = new Extractor(); // Aca podría faltar el terreno
-        Zangano zangano = new Zangano();
-        Zangano zangano2 = new Zangano();
+      Terreno terrenoVolcan = new TerrenoVolcan(new Coordenada( 1,2));
+      Extractor extractor = new Extractor();
+      Inventario inv = new Inventario(new GasVespeno(0), new Mineral(0));
 
-        extractor.ingresarUnidad(zangano);
-        extractor.ingresarUnidad(zangano2);
+      int tiempoDeConstruccion = 6;
+      extractor.ocupar(terrenoVolcan);
 
-        GasVespeno gas = extractor.extraerRecursos();
+      for (int i = 0; i < tiempoDeConstruccion; i++) {
+          extractor.actualizar();
+      }
 
-        Assertions.assertThrows(RecursosInsuficientes.class,()->{
-            gas.gastar(new GasVespeno(30));
-        });
+      extractor.ingresarUnidad(new Zangano());
+      extractor.ingresarUnidad(new Zangano());
+
+      extractor.recolectarRecursos(inv);
+
+      inv.consumirGasVespeno(new GasVespeno(20));
+
+      Assertions.assertThrows(RecursosInsuficientes.class, ()->{
+          inv.consumirGasVespeno(new GasVespeno(1));
+      });
     }
 
     @Test
     public void extractorCon3ZanganoGenera30UnidadesDeGas() {
-        Extractor extractor = new Extractor(); // Aca podría faltar el terreno
-        Zangano zangano = new Zangano();
-        Zangano zangano2 = new Zangano();
-        Zangano zangano3 = new Zangano();
+      Terreno terrenoVolcan = new TerrenoVolcan(new Coordenada( 1,2));
+      Extractor extractor = new Extractor();
+      Inventario inv = new Inventario(new GasVespeno(0), new Mineral(0));
 
-        extractor.ingresarUnidad(zangano);
-        extractor.ingresarUnidad(zangano2);
-        extractor.ingresarUnidad(zangano3);
+      int tiempoDeConstruccion = 6;
+      extractor.ocupar(terrenoVolcan);
 
-        GasVespeno gas = extractor.extraerRecursos();
+      for (int i = 0; i < tiempoDeConstruccion; i++) {
+          extractor.actualizar();
+      }
 
-        Assertions.assertThrows(RecursosInsuficientes.class,()->{
-            gas.gastar(new GasVespeno(40));
-        });
+      extractor.ingresarUnidad(new Zangano());
+      extractor.ingresarUnidad(new Zangano());
+      extractor.ingresarUnidad(new Zangano());
 
+      extractor.recolectarRecursos(inv);
+
+      inv.consumirGasVespeno(new GasVespeno(30));
+
+      Assertions.assertThrows(RecursosInsuficientes.class, ()->{
+          inv.consumirGasVespeno(new GasVespeno(1));
+      });
     }
 
-    */
+    @Test
+    public void asimiladorGenera20UnidadesDeGas() {
+      Terreno terrenoVolcan = new TerrenoVolcan(new Coordenada( 1,2));
+      Asimilador asimilador = new Asimilador();
+      Inventario inv = new Inventario(new GasVespeno(0), new Mineral(0));
 
+      int tiempoDeConstruccion = 6;
+      asimilador.ocupar(terrenoVolcan);
+
+      for (int i = 0; i < tiempoDeConstruccion; i++) {
+        asimilador.actualizar();
+      }
+
+      asimilador.recolectarRecursos(inv);
+
+      inv.consumirGasVespeno(new GasVespeno(20));
+
+      Assertions.assertThrows(RecursosInsuficientes.class, ()->{
+          inv.consumirGasVespeno(new GasVespeno(1));
+      });
+    }
 }
