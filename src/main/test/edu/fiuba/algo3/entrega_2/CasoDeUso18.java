16c3636de88c79ad603f0d9b05dc61702bde19da
Lucas Barrientos <lucasmabarrientos@gmail.com>
2022-11-21 09:10:46 -0300

    acomodar caso 18 en la carpeta que corresponde

--- src/main/test/edu/fiuba/algo3/entrega_2/CasoDeUso18.java
+++ src/main/test/edu/fiuba/algo3/entrega_2/CasoDeUso18.java
@@ -1,18 +1,119 @@
 package edu.fiuba.algo3.entrega_2;
 
+import edu.fiuba.algo3.modelo.AlgoStar;
+import edu.fiuba.algo3.modelo.edificios.protoss.pilon.Pilon;
 import edu.fiuba.algo3.modelo.edificios.zerg.criadero.Criadero;
-import edu.fiuba.algo3.modelo.geometria.Coordenada;
+import edu.fiuba.algo3.modelo.terrenos.*;
+import edu.fiuba.algo3.modelo.terrenos.TerrenoVolcan;
+import edu.fiuba.algo3.modelo.unidades.protoss.Dragon;
+import edu.fiuba.algo3.modelo.unidades.protoss.Scout;
+import edu.fiuba.algo3.modelo.unidades.protoss.Zealot;
+import edu.fiuba.algo3.modelo.unidades.zerg.Guardian;
+import edu.fiuba.algo3.modelo.unidades.zerg.Hidralisco;
+import edu.fiuba.algo3.modelo.unidades.zerg.Mutalisco;
+import edu.fiuba.algo3.modelo.unidades.zerg.Zerling;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class CasoDeUso18 {
+/*
     @Test
-    public void verificarQueLasUnidadesCausenDañoCorrecto(){
-        Criadero criadero = new Criadero();
-        for(int i=0; i<5; i++) { // paso los turnos para que se termine de construir
-            criadero.actualizar();
-        }
+    public void zerlingAtacaAPilon150VecesYALaProximaDevuelveErrorPorEdificioDestruido() {
+      Zerling zerling = new Zerling();
+      Pilon pilon = new Pilon();
 
+      for(int i = 0; i < 150; i++) {
+        zerling.atacar(pilon);
+      }
 
+      Assertions.assertThrows(EdificioDestruido.class, ()->{
+        zerling.atacar(pilon);
+      });
     }
+
+    @Test
+    public void hidraliscoAtacaAPilon60VecesYALaProximaDevuelveErrorPorEdificioDestruido() {
+      Hidralisco hidralisco = new Hidralisco();
+      Pilon pilon = new Pilon();
+
+      for(int i = 0; i < 60; i++) {
+        hidralisco.atacar(pilon);
+      }
+
+      Assertions.assertThrows(EdificioDestruido.class, ()->{
+        hidralisco.atacar(pilon);
+      });
+    }
+
+    @Test
+    public void mutaliscoAtacaAPilon66VecesYALaProximaDevuelveErrorPorEdificioDestruido() {
+      Mutalisco mutalisco = new Mutalisco();
+      Pilon pilon = new Pilon();
+
+      for(int i = 0; i < 66; i++) {
+        mutalisco.atacar(pilon);
+      }
+
+      Assertions.assertThrows(EdificioDestruido.class, ()->{
+        mutalisco.atacar(pilon);
+      });
+    }
+
+    @Test
+    public void guardianAtacaAPilon24VecesYALaProximaDevuelveErrorPorEdificioDestruido() {
+      Guardian guardian = new Guardian();
+      Pilon pilon = new Pilon();
+
+      for(int i = 0; i < 24; i++) {
+        guardian.atacar(pilon);
+      }
+
+      Assertions.assertThrows(EdificioDestruido.class, ()->{
+        guardian.atacar(pilon);
+      });
+    }
+
+    @Test
+    public void zealotAtacaACriadero62VecesYALaProximaDevuelveErrorPorEdificioDestruido() {
+      Zealot zealot = new Zealot();
+      Criadero criadero = new Criadero();
+
+      for(int i = 0; i < 62; i++) {
+        zealot.atacar(criadero);
+      }
+
+      Assertions.assertThrows(EdificioDestruido.class, ()->{
+        zealot.atacar(criadero);
+      });
+    }
+
+    @Test
+    public void dragonAtacaACriadero25VecesYALaProximaDevuelveErrorPorEdificioDestruido() {
+      Dragon dragon = new Dragon();
+      Criadero criadero = new Criadero();
+
+      for(int i = 0; i < 25; i++) {
+        dragon.atacar(criadero);
+      }
+
+      Assertions.assertThrows(EdificioDestruido.class, ()->{
+        dragon.atacar(criadero);
+      });
+    }
+
+    @Test
+    public void scoutAtacaACriadero62VecesYALaProximaDevuelveErrorPorEdificioDestruido() {
+      Scout scout = new Scout();
+      Criadero criadero = new Criadero();
+
+      for(int i = 0; i < 62; i++) {
+        scout.atacar(criadero);
+      }
+
+      Assertions.assertThrows(EdificioDestruido.class, ()->{
+        scout.atacar(criadero);
+      });
+    }
+ */
 }
