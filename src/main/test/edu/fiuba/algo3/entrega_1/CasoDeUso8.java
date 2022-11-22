201e6569a7ab1f33ff0d55d1916b379006fa5a3e
eze <eze.f.tec@gmail.com>
2022-11-22 03:25:45 -0300

    8 full full

--- src/main/test/edu/fiuba/algo3/entrega_1/CasoDeUso8.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/CasoDeUso8.java
@@ -1,6 +1,12 @@
 package edu.fiuba.algo3.entrega_1;
 
 import edu.fiuba.algo3.modelo.*;
+import edu.fiuba.algo3.modelo.edificios.Edificio;
+import edu.fiuba.algo3.modelo.edificios.zerg.criadero.Criadero;
+import edu.fiuba.algo3.modelo.edificios.zerg.espiral.Espiral;
+import edu.fiuba.algo3.modelo.edificios.zerg.extractor.Extractor;
+import edu.fiuba.algo3.modelo.edificios.zerg.guarida.Guarida;
+import edu.fiuba.algo3.modelo.edificios.zerg.reservadeReproduccion.ReservaDeReproduccion;
 import edu.fiuba.algo3.modelo.jugadores.Inventario;
 import edu.fiuba.algo3.modelo.recursos.GasVespeno;
 
@@ -37,7 +43,7 @@ public class CasoDeUso8 {
 
   @Test
   public void sePuedeConstruirNexoMineralcon50Minerales() {
-    Inventario inventario = new Inventario(new GasVespeno(0), new Mineral(60));
+    Inventario inventario = new Inventario(new GasVespeno(0), new Mineral(50));
     NexoMineral nexoMineral = new NexoMineral();
     nexoMineral.consumirRecursosParaConstruccion(inventario);
 
@@ -51,4 +57,244 @@ public class CasoDeUso8 {
       nexoMineral2.consumirRecursosParaConstruccion(inventario);
     });
   }
+  @Test
+  public void noSePuedeConstruirPilonconMenosDe100Minerales() {
+    Inventario inventario = new Inventario(new GasVespeno(0), new Mineral(99));
+    Edificio edificio = new Pilon();
+
+    Assertions.assertThrows(RecursosInsuficientes.class, ()->{
+      edificio.consumirRecursosParaConstruccion(inventario);
+    });
+  }
+
+  @Test
+  public void sePuedeConstruirPiloncon100Minerales() {
+    Inventario inventario = new Inventario(new GasVespeno(0), new Mineral(100));
+    Edificio edificio = new Pilon();
+    edificio.consumirRecursosParaConstruccion(inventario);
+
+    for(int i=0; i< 4; i++ ){
+      edificio.actualizar(inventario); //paso los turnos para terminar de construir el nexo
+    }
+
+    Edificio edificioNuevo = new Pilon();
+
+    Assertions.assertThrows(RecursosInsuficientes.class, ()->{
+      edificioNuevo.consumirRecursosParaConstruccion(inventario);
+    });
+  }
+  @Test
+  public void noSePuedeConstruirAsimiladorconMenosDe100Minerales() {
+    Inventario inventario = new Inventario(new GasVespeno(0), new Mineral(99));
+    Edificio edificio = new Asimilador();
+
+    Assertions.assertThrows(RecursosInsuficientes.class, ()->{
+      edificio.consumirRecursosParaConstruccion(inventario);
+    });
+  }
+
+  @Test
+  public void sePuedeConstruirAsimiladorcon100Minerales() {
+    Inventario inventario = new Inventario(new GasVespeno(0), new Mineral(100));
+    Edificio edificio = new Asimilador();
+    edificio.consumirRecursosParaConstruccion(inventario);
+
+    for(int i=0; i< 4; i++ ){
+      edificio.actualizar(inventario); //paso los turnos para terminar de construir el nexo
+    }
+
+    Edificio edificioNuevo = new Asimilador();
+
+    Assertions.assertThrows(RecursosInsuficientes.class, ()->{
+      edificioNuevo.consumirRecursosParaConstruccion(inventario);
+    });
+  }
+  @Test
+  public void noSePuedeConstruirAccesoconMenosDe150Minerales() {
+    Inventario inventario = new Inventario(new GasVespeno(0), new Mineral(149));
+    Edificio edificio = new Acceso();
+
+    Assertions.assertThrows(RecursosInsuficientes.class, ()->{
+      edificio.consumirRecursosParaConstruccion(inventario);
+    });
+  }
+
+  @Test
+  public void sePuedeConstruirAccesocon150Minerales() {
+    Inventario inventario = new Inventario(new GasVespeno(0), new Mineral(150));
+    Edificio edificio = new Acceso();
+    edificio.consumirRecursosParaConstruccion(inventario);
+
+    for(int i=0; i< 4; i++ ){
+      edificio.actualizar(inventario); //paso los turnos para terminar de construir el nexo
+    }
+
+    Edificio edificioNuevo = new Acceso();
+
+    Assertions.assertThrows(RecursosInsuficientes.class, ()->{
+      edificioNuevo.consumirRecursosParaConstruccion(inventario);
+    });
+  }
+  @Test
+  public void noSePuedeConstruirGuaridaconMenosDe150MineralesY150GasVespeno() {
+    Inventario inventario = new Inventario(new GasVespeno(100), new Mineral(149));
+    Edificio edificio = new PuertoEstelar();
+
+    Assertions.assertThrows(RecursosInsuficientes.class, ()->{
+      edificio.consumirRecursosParaConstruccion(inventario);
+    });
+  }
+
+  @Test
+  public void sePuedeConstruirGuaridacon150MineralesY150GasVespeno() {
+    Inventario inventario = new Inventario(new GasVespeno(150), new Mineral(150));
+    Edificio edificio = new PuertoEstelar();
+    edificio.consumirRecursosParaConstruccion(inventario);
+
+    for (int i = 0; i < 4; i++) {
+      edificio.actualizar(inventario); //paso los turnos para terminar de construir el nexo
+    }
+
+    Edificio edificioNuevo = new PuertoEstelar();
+
+    Assertions.assertThrows(RecursosInsuficientes.class, () -> {
+      edificioNuevo.consumirRecursosParaConstruccion(inventario);
+    });
+  }
+  @Test
+  public void noSePuedeConstruirCriaderoconMenosDe200Minerales() {
+    Inventario inventario = new Inventario(new GasVespeno(0), new Mineral(199));
+    Edificio edificio = new Criadero();
+
+    Assertions.assertThrows(RecursosInsuficientes.class, ()->{
+      edificio.consumirRecursosParaConstruccion(inventario);
+    });
+  }
+
+  @Test
+  public void sePuedeConstruirCriaderocon200Minerales() {
+    Inventario inventario = new Inventario(new GasVespeno(0), new Mineral(200));
+    Edificio edificio = new Criadero();
+    edificio.consumirRecursosParaConstruccion(inventario);
+
+    for(int i=0; i< 4; i++ ){
+      edificio.actualizar(inventario); //paso los turnos para terminar de construir el nexo
+    }
+
+    Edificio edificioNuevo = new Criadero();
+
+    Assertions.assertThrows(RecursosInsuficientes.class, ()->{
+      edificioNuevo.consumirRecursosParaConstruccion(inventario);
+    });
+  }
+  @Test
+  public void noSePuedeConstruirExtractorconMenosDe100Minerales() {
+    Inventario inventario = new Inventario(new GasVespeno(0), new Mineral(99));
+    Edificio edificio = new Extractor();
+
+    Assertions.assertThrows(RecursosInsuficientes.class, ()->{
+      edificio.consumirRecursosParaConstruccion(inventario);
+    });
+  }
+
+  @Test
+  public void sePuedeConstruirExtractorcon100Minerales() {
+    Inventario inventario = new Inventario(new GasVespeno(0), new Mineral(100));
+    Edificio edificio = new Extractor();
+    edificio.consumirRecursosParaConstruccion(inventario);
+
+    for(int i=0; i< 4; i++ ){
+      edificio.actualizar(inventario); //paso los turnos para terminar de construir el nexo
+    }
+
+    Edificio edificioNuevo = new Extractor();
+
+    Assertions.assertThrows(RecursosInsuficientes.class, ()->{
+      edificioNuevo.consumirRecursosParaConstruccion(inventario);
+    });
+  }
+  @Test
+  public void noSePuedeConstruirReservaDeReproduccionconMenosDe150Minerales() {
+    Inventario inventario = new Inventario(new GasVespeno(0), new Mineral(149));
+    Edificio edificio = new ReservaDeReproduccion();
+
+    Assertions.assertThrows(RecursosInsuficientes.class, ()->{
+      edificio.consumirRecursosParaConstruccion(inventario);
+    });
+  }
+
+  @Test
+  public void sePuedeConstruirReservaDeReproduccioncon150Minerales() {
+    Inventario inventario = new Inventario(new GasVespeno(0), new Mineral(150));
+    Edificio edificio = new ReservaDeReproduccion();
+    edificio.consumirRecursosParaConstruccion(inventario);
+
+    for(int i=0; i< 4; i++ ){
+      edificio.actualizar(inventario); //paso los turnos para terminar de construir el nexo
+    }
+
+    Edificio edificioNuevo = new ReservaDeReproduccion();
+
+    Assertions.assertThrows(RecursosInsuficientes.class, ()->{
+      edificioNuevo.consumirRecursosParaConstruccion(inventario);
+    });
+  }
+  @Test
+  public void noSePuedeConstruirEspiralconMenosDe150MineralesY100GasVespeno() {
+    Inventario inventario = new Inventario(new GasVespeno(100), new Mineral(149));
+    Edificio edificio = new Espiral();
+
+    Assertions.assertThrows(RecursosInsuficientes.class, ()->{
+      edificio.consumirRecursosParaConstruccion(inventario);
+    });
+  }
+
+  @Test
+  public void sePuedeConstruirEspiralcon150MineralesY100GasVespeno() {
+    Inventario inventario = new Inventario(new GasVespeno(100), new Mineral(150));
+    Edificio edificio = new Espiral();
+    edificio.consumirRecursosParaConstruccion(inventario);
+
+    for(int i=0; i< 4; i++ ){
+      edificio.actualizar(inventario); //paso los turnos para terminar de construir el nexo
+    }
+
+    Edificio edificioNuevo = new Espiral();
+
+    Assertions.assertThrows(RecursosInsuficientes.class, ()->{
+      edificioNuevo.consumirRecursosParaConstruccion(inventario);
+    });
+  }
+
+  @Test
+  public void noSePuedeConstruirGuaridaconMenosDe150MineralesY100GasVespeno() {
+    Inventario inventario = new Inventario(new GasVespeno(100), new Mineral(199));
+    Edificio edificio = new Guarida();
+
+    Assertions.assertThrows(RecursosInsuficientes.class, ()->{
+      edificio.consumirRecursosParaConstruccion(inventario);
+    });
+  }
+
+  @Test
+  public void sePuedeConstruirGuaridacon150MineralesY100GasVespeno() {
+    Inventario inventario = new Inventario(new GasVespeno(100), new Mineral(200));
+    Edificio edificio = new Guarida();
+    edificio.consumirRecursosParaConstruccion(inventario);
+
+    for(int i=0; i< 4; i++ ){
+      edificio.actualizar(inventario); //paso los turnos para terminar de construir el nexo
+    }
+
+    Edificio edificioNuevo = new Guarida();
+
+    Assertions.assertThrows(RecursosInsuficientes.class, ()->{
+      edificioNuevo.consumirRecursosParaConstruccion(inventario);
+    });
+  }
+
+
+
+
 }
+
