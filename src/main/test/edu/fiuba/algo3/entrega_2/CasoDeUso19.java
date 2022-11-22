e3f35a4adbf73261e3a8be7fd9bbe30e471ce99e
amurseli <86434411+amurseli@users.noreply.github.com>
2022-11-22 20:29:55 -0300

    Update CasoDeUso19.java

--- src/main/test/edu/fiuba/algo3/entrega_2/CasoDeUso19.java
+++ src/main/test/edu/fiuba/algo3/entrega_2/CasoDeUso19.java
@@ -1,5 +1,7 @@
 package edu.fiuba.algo3.entrega_2;
 
+import edu.fiuba.algo3.modelo.excepciones.UnidadDestruida;
+import edu.fiuba.algo3.modelo.geometria.direcciones.Derecha;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -14,26 +16,26 @@ import edu.fiuba.algo3.modelo.unidades.zerg.Zerling;
 public class CasoDeUso19 {
 
     @Test
-    public void unZerlingPuedeAtacarAlZealotPeroNoAlScout() {
+    public void unZerlingPuedeAtacarAlScoutperoNoLeHaceDaño() {
         Mapa mapa = new Mapa(new Coordenada(100, 50));
         Zerling zerling = new Zerling();
-        Zealot zealot = new Zealot();
         Scout scout = new Scout();
 
         mapa.establecerUnidad(new Coordenada(2, 1), zerling);
-        mapa.establecerUnidad(new Coordenada(3, 1), zealot);
         mapa.establecerUnidad(new Coordenada(2, 2), scout);
 
         for (int i=0; i<20; i++) {
             zerling.actualizar();
-            zealot.actualizar();
             scout.actualizar();
         }
 
-        zerling.atacar(new Coordenada(3, 1), mapa);
+        for (int i = 0; i < 200; i++) {
 
-        Assertions.assertThrows(AtaqueImposibleDeRealizarse.class, ()->{
             zerling.atacar(new Coordenada(2, 2), mapa);
+        }
+
+        Assertions.assertThrows(UnidadDestruida.class, ()->{
+            scout.moverse(new Derecha(),mapa);
         });
     }
     
