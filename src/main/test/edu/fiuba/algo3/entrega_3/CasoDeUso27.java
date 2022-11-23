4190ada3a0651ab8ebc068492dd0f67469f184f0
amurseli <86434411+amurseli@users.noreply.github.com>
2022-11-22 22:01:47 -0300

    Update CasoDeUso27.java

--- src/main/test/edu/fiuba/algo3/entrega_3/CasoDeUso27.java
+++ src/main/test/edu/fiuba/algo3/entrega_3/CasoDeUso27.java
@@ -1,35 +1,46 @@
 package edu.fiuba.algo3.entrega_3;
 
 import edu.fiuba.algo3.modelo.AlgoStar;
+import edu.fiuba.algo3.modelo.edificios.protoss.acceso.Acceso;
+import edu.fiuba.algo3.modelo.edificios.protoss.pilon.Pilon;
+import edu.fiuba.algo3.modelo.edificios.protoss.puertoEstelar.PuertoEstelar;
 import edu.fiuba.algo3.modelo.edificios.zerg.criadero.Criadero;
 import edu.fiuba.algo3.modelo.edificios.zerg.espiral.Espiral;
+import edu.fiuba.algo3.modelo.edificios.zerg.guarida.Guarida;
 import edu.fiuba.algo3.modelo.edificios.zerg.reservadeReproduccion.ReservaDeReproduccion;
+import edu.fiuba.algo3.modelo.excepciones.EdificioEstaDestruido;
+import edu.fiuba.algo3.modelo.excepciones.UnidadEstaDestruida;
 import edu.fiuba.algo3.modelo.geometria.Coordenada;
+import edu.fiuba.algo3.modelo.geometria.direcciones.Abajo;
 import edu.fiuba.algo3.modelo.geometria.direcciones.Derecha;
 import edu.fiuba.algo3.modelo.jugadores.JugadorProtoss;
 import edu.fiuba.algo3.modelo.jugadores.JugadorZerg;
+import edu.fiuba.algo3.modelo.unidades.UnidadDestruida;
+import edu.fiuba.algo3.modelo.unidades.protoss.Scout;
+import edu.fiuba.algo3.modelo.unidades.zerg.Devorador;
 import edu.fiuba.algo3.modelo.unidades.zerg.Mutalisco;
 import edu.fiuba.algo3.modelo.unidades.zerg.Zangano;
 
 import org.junit.jupiter.api.Test;
-
-import com.tngtech.archunit.lang.ArchRule.Assertions;
+import org.junit.jupiter.api.Assertions;
 
 public class CasoDeUso27 {
 
-    /*
+/*
 
     @Test
     public void unMutalizcoPuedeEvolucionarEnUnDevoradorSiHaySuficientesRecursosYLuegoPuedeAtacarAUnEnemigoA5CoordenadasDeDistancia() {
         AlgoStar algoStar = new AlgoStar();
-        JugadorZerg jugadorZerg = new JugadorZerg("La mente suprema", "#ff0000", 800, 800); // TODO: Marcar los recursos estrictamente necesarios para lograr pasar esta prueba
+        JugadorZerg jugadorZerg = new JugadorZerg("La mente suprema", "#ff0000", 1200, 1200); // TODO: Marcar los recursos estrictamente necesarios para lograr pasar esta prueba
         algoStar.agregarJugador(jugadorZerg);
-        JugadorProtoss jugadorProtoss = new JugadorProtoss("El primogenito", "#0000ff");
+        JugadorProtoss jugadorProtoss = new JugadorProtoss("El primogenito", "#0000ff",1200, 1200);
         algoStar.agregarJugador(jugadorProtoss);
         algoStar.empezarJuego();
 
         jugadorZerg.construirEdificio(new Coordenada(1, 1), new Criadero());
-        for (int i = 0; i < 8; i++) {
+        algoStar.pasarTurno();
+        jugadorProtoss.construirEdificio(new Coordenada(8,3), new Pilon());
+        for (int i = 0; i < 7; i++) {
             algoStar.pasarTurno();
         }
 
@@ -76,19 +87,34 @@ public class CasoDeUso27 {
         }
 
         // Evolucionar el mutalisco en un devorador
-        jugadorZerg.evolucionarUnidad(new Coordenada(3,1), new Devorador());
+        jugadorZerg.evolucionar(new Coordenada(3,1), new Devorador());
         for (int i = 0; i < 8; i++) {
             algoStar.pasarTurno();
         }
 
+
+        algoStar.pasarTurno();
+        jugadorProtoss.construirEdificio(new Coordenada(8,4), new Acceso());
+        for (int i = 0; i <14; i++) {
+            algoStar.pasarTurno();
+        }
+
+        jugadorProtoss.construirEdificio(new Coordenada(8,2), new PuertoEstelar());
+        for (int i = 0; i <14; i++) {
+            algoStar.pasarTurno();
+        }
+        jugadorProtoss.generarUnidad(new Coordenada(8,2),new Scout());
+        for (int i = 0; i <14; i++) {
+            algoStar.pasarTurno();
+        }
         // TODO: jugadorProtoss debe construir una unidad en la coordenada (8,1)
 
+
+        jugadorZerg.atacar(new Coordenada(3,1), new Coordenada(8,1));
+
         // Se intenta atacar una unidad que esta a 5 de rango
-        Assertions.assertThrows(UnidadDestruida.class, ()->{
+        Assertions.assertThrows(EdificioEstaDestruido.class, ()->{
             jugadorZerg.atacar(new Coordenada(3,1), new Coordenada(8,1));
         });
-    }
-
-    */
-
+    }*/
 }
