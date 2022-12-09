43663655cdb68d30cdbbdf0e033da054cf983209
amurseli <amurseli@fi.uba.ar>
2022-12-09 20:02:20 -0300

    Update Logger.java

--- src/main/java/edu/fiuba/algo3/modelo/Logger.java
+++ src/main/java/edu/fiuba/algo3/modelo/Logger.java
@@ -2,7 +2,19 @@ package edu.fiuba.algo3.modelo;
 
 public class Logger {
 
+    static Logger log;
+
+    private Logger (){
+
+    }
     public static void log(String log){
         System.out.println(log);
+    }
+
+    public static Logger getInstance(){
+        if (log == null){
+            log = new Logger();
+        }
+        return log;
     }
 }
