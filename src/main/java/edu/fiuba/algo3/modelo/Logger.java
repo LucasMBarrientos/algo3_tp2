98ca43fbebd5960b7469ee0dc692eae0f4715174
amurseli <amurseli@fi.uba.ar>
2022-12-10 18:26:39 -0300

    Cambios en logger y en prueba de integracion

--- src/main/java/edu/fiuba/algo3/modelo/Logger.java
+++ src/main/java/edu/fiuba/algo3/modelo/Logger.java
@@ -3,12 +3,19 @@ package edu.fiuba.algo3.modelo;
 public class Logger {
 
     static Logger log;
+    public static Boolean enableLog = false;
 
     private Logger (){
 
     }
     public static void log(String log){
-        System.out.println(log);
+        if(enableLog){
+            System.out.println(log);
+        }
+    }
+
+    public static void setEnableLog(Boolean value){
+        enableLog = value;
     }
 
     public static Logger getInstance(){
--- src/main/test/edu/fiuba/algo3/pruebas_de_integracion/prueba1.java
+++ src/main/test/edu/fiuba/algo3/pruebas_de_integracion/prueba1.java
@@ -20,53 +20,41 @@ public class prueba1 {
 
         AlgoStar a = new AlgoStar();
 
-
-        Logger.log("El Jugador 1 Elige raza Zerg, nombre Zerggg y color #ff0000");
         a.agregarJugador(new JugadorZerg("Zerggg","#ff0000"));
 
-        Logger.log("El Jugador 1 Elige raza Protoss, nombre Protoss y color #0000ff");
         a.agregarJugador(new JugadorProtoss("Protoss","#0000ff"));
 
-        Logger.log("Inicia el Juego, Empieza la Ronda 1");
         a.empezarJuego();
 
-        Logger.log("El jugador 1 mueve su zangano incial hacia la coordenada (4,3), donde se encuentra un Mineral.");
         a.hallarJugadorActual().moverUnidad(new Coordenada(1,1), new Derecha());
         a.hallarJugadorActual().moverUnidad(new Coordenada(2,1), new Derecha());
         a.hallarJugadorActual().moverUnidad(new Coordenada(3,1), new Derecha());
         a.hallarJugadorActual().moverUnidad(new Coordenada(4,1), new Abajo());
         a.hallarJugadorActual().moverUnidad(new Coordenada(4,2), new Abajo());
 
-        Logger.log("El jugador 1 pasa el turno");
         a.pasarTurno();
 
-        Logger.log("Inicia el turno del Jugador Protoss");
-        Logger.log("El jugador Protoss construye un nexo mineral en la coordenada (45,46)");
-        a.hallarJugadorActual().construirEdificio(new Coordenada(45,46), new NexoMineral());
-        Logger.log("El jugador Protoss construye un asimilador en la coordenada (45,45)");
-        a.hallarJugadorActual().construirEdificio(new Coordenada(45,45), new Asimilador());
+        a.hallarJugadorActual().construirEdificio(new Coordenada(95 ,46), new NexoMineral());
+        a.hallarJugadorActual().construirEdificio(new Coordenada(95,45), new Asimilador());
 
-        Logger.log("El jugador 2 pasa el turno");
-        Logger.log("Inicia el Juego, Empieza la Ronda 2");
         a.pasarTurno();
-        Logger.log("El jugador 1 pasa el turno");
         a.pasarTurno();
-        Logger.log("El jugador 2 pasa el turno");
-        Logger.log("Inicia el Juego, Empieza la Ronda 3");
         a.pasarTurno();
-        Logger.log("El jugador 1 pasa el turno");
         a.pasarTurno();
-        Logger.log("El jugador 2 pasa el turno");
-        Logger.log("Inicia el Juego, Empieza la Ronda 4");
         a.pasarTurno();
-        Logger.log("El jugador 1 pasa el turno");
+        a.pasarTurno();
+        a.pasarTurno();
+        a.pasarTurno();
+        a.pasarTurno();
+        a.pasarTurno();
+        a.pasarTurno();
+        a.pasarTurno();
+        a.pasarTurno();
         a.pasarTurno();
 
-        //Logger.log("El jugador Protoss construye un pilon en la coordenada (6,6)");
-       // a.hallarJugadorActual().construirEdificio(new Coordenada(6,6),new Pilon());
+        a.hallarJugadorActual().construirEdificio(new Coordenada(6,2),new Pilon());
 
-
-
+        a.pasarTurno();
 
     }
 
