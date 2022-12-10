ff2859c075766bbd2fc0761a3eb4a696fe624cf7
amurseli <amurseli@fi.uba.ar>
2022-12-10 18:37:45 -0300

    Update prueba1.java

--- src/main/test/edu/fiuba/algo3/pruebas_de_integracion/prueba1.java
+++ src/main/test/edu/fiuba/algo3/pruebas_de_integracion/prueba1.java
@@ -2,15 +2,18 @@ package edu.fiuba.algo3.pruebas_de_integracion;
 
 import edu.fiuba.algo3.modelo.AlgoStar;
 import edu.fiuba.algo3.modelo.Logger;
+import edu.fiuba.algo3.modelo.edificios.protoss.Acceso;
 import edu.fiuba.algo3.modelo.edificios.protoss.Asimilador;
 import edu.fiuba.algo3.modelo.edificios.protoss.NexoMineral;
 import edu.fiuba.algo3.modelo.edificios.protoss.Pilon;
+import edu.fiuba.algo3.modelo.edificios.zerg.Criadero;
 import edu.fiuba.algo3.modelo.geometria.Coordenada;
 import edu.fiuba.algo3.modelo.geometria.direcciones.Abajo;
 import edu.fiuba.algo3.modelo.geometria.direcciones.Derecha;
 import edu.fiuba.algo3.modelo.jugadores.Inventario;
 import edu.fiuba.algo3.modelo.jugadores.JugadorProtoss;
 import edu.fiuba.algo3.modelo.jugadores.JugadorZerg;
+import edu.fiuba.algo3.modelo.unidades.zerg.Zangano;
 import org.junit.jupiter.api.Test;
 
 public class prueba1 {
@@ -55,6 +58,21 @@ public class prueba1 {
         a.hallarJugadorActual().construirEdificio(new Coordenada(6,2),new Pilon());
 
         a.pasarTurno();
+
+        a.hallarJugadorActual().moverUnidad(new Coordenada(4,3), new Derecha());
+        a.hallarJugadorActual().construirEdificio(new Coordenada(5,3),new Criadero());
+
+        for (int i = 0; i < 5; i++) {
+            a.pasarTurno();
+        }
+
+        a.hallarJugadorActual().construirEdificio(new Coordenada(5,2), new Acceso());
+
+        a.pasarTurno();
+
+        a.hallarJugadorActual().generarUnidad(new Coordenada(5,3), new Zangano());
+        a.hallarJugadorActual().generarUnidad(new Coordenada(5,3), new Zangano());
+        a.hallarJugadorActual().generarUnidad(new Coordenada(5,3), new Zangano());
 
     }
 
